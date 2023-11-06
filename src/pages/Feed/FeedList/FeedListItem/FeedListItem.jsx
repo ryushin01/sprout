4ec88656d48c4e2d172ff87bd2bec5d8a009d373
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-11-06 19:30:51 +0900

    [style] 피드 2차 범위까지 UI 완료

--- src/pages/Feed/FeedList/FeedListItem/FeedListItem.jsx
+++ src/pages/Feed/FeedList/FeedListItem/FeedListItem.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { ReactComponent as IconPrevArrow } from '../../../../assets/images/icon_prev_arrow.svg';
 import { ReactComponent as IconNextArrow } from '../../../../assets/images/icon_next_arrow.svg';
+import Portal from '../../../../components/Modal/Portal';
+import Modal from '../../../../components/Modal/Modal';
 import BIG_BANNER_SWIPER_DATA from '../../../../data/BigBannerSwiperData';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
@@ -9,12 +12,34 @@ import 'swiper/css/pagination';
 import './FeedListItem.scss';
 
 const FeedListItem = () => {
+  const defaultProfileImage = '/images/feed/default_profile_image.png';
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const modalHandler = () => {
+    setModalOpen(prev => !prev);
+  };
+
+  useEffect(() => {
+    const close = e => {
+      if (e.keyCode === 27) {
+        setModalOpen(false);
+      }
+    };
+
+    window.addEventListener('keydown', close);
+  }, []);
+
+  const profileImage = null;
+
   return (
     <article className="feed-list-item">
       <div>
         <div className="info">
           <div className="left-split">
-            <img src="images/feed/default_profile_image.png" alt="" />
+            <img
+              src={profileImage === null ? defaultProfileImage : profileImage}
+              alt=""
+            />
             <strong>닉네임</strong>
           </div>
           <div className="right-split">
@@ -56,11 +81,35 @@ const FeedListItem = () => {
               </div>
             </Swiper>
           </div>
-          <p>피드 내용 텍스트</p>
+          <p>
+            피드 내용이 텍스트로 들어갑니다. 최대 3줄까지 노출되며 모달
+            팝업에서는 전체 내용이 드러납니다. 피드 내용이 텍스트로 들어갑니다.
+            최대 3줄까지 노출되며 모달 팝업에서는 전체 내용이 드러납니다. 피드
+            내용이 텍스트로 들어갑니다. 최대 3줄까지 노출되며 모달 팝업에서는
+            전체 내용이 드러납니다. 피드 내용이 텍스트로 들어갑니다. 최대
+            3줄까지 노출되며 모달 팝업에서는 전체 내용이 드러납니다. 피드 내용이
+            텍스트로 들어갑니다. 최대 3줄까지 노출되며 모달 팝업에서는 전체
+            내용이 드러납니다.
+          </p>
         </div>
 
-        <div>interests</div>
-        <div>comments</div>
+        <div className="comment">
+          <button type="button" onClick={modalHandler}>
+            댓글 n개 모두 보기
+          </button>
+          <Portal>
+            {modalOpen && (
+              <Modal
+                // data={<Charge points={points} onClose={modalHandler} />}
+                onClose={modalHandler}
+              />
+            )}
+          </Portal>
+        </div>
+
+        <div>
+          [3차] 댓글 달기 영역: 로그인한 프로필 이미지 + textarea 처리 필요
+        </div>
       </div>
     </article>
   );
